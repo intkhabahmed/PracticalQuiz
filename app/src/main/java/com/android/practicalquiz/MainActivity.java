39c072381438e7356a850ab529a6681eb3e74efd
Intkhab Ahmed <intkhab.ahmed64@gmail.com>
2018-04-21 23:13:10 +0530

    Edited main activity layout and menu with drawables

--- app/src/main/java/com/android/practicalquiz/MainActivity.java
+++ app/src/main/java/com/android/practicalquiz/MainActivity.java
@@ -2,6 +2,9 @@ package com.android.practicalquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void openDetail(View view) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
